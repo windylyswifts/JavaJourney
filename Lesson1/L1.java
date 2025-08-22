dee8e8adcf497165106912a4a25069b6e965272d
windylyswifts <isiahalinab@gmail.com>
2025-08-22 20:14:30 +0800

    Start of the Journey

---
+++ Lesson1/L1.java
@@ -0,0 +1,9 @@
+public class L1 {
+    public static void main(String[] args) {
+        System.out.println("I like Gaming"); //println is basically make a new line
+        System.out.print("I will finish this sooner than expected\n"); // \n works the same
+        System.out.println("I will finish this sooner than fiel!\n"); //println is basically make a new line
+        //we can also type out sout so that we dont have to manually type it out
+        
+    }
+}
