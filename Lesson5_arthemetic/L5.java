ef5a346e25ee3ca42f52a6f919002efb6fafab41
windylyswifts <isiahalinab@gmail.com>
2025-08-24 10:50:08 +0800

    finished lesson 5, arthemetic

---
+++ Lesson5_arthemetic/L5.java
@@ -0,0 +1,16 @@
+package Lesson5_arthemetic;
+
+public class L5 {
+    public static void main(String[] args) {
+    
+        //I dont need to go through all of the arthemetic operation because I already leared this on my C language
+        //Journey and its the same for this language
+        
+        //ORDER OF OPERATIONS [P-E-M-D-A-S]
+
+        double result = 3 + 4 * (7-5) /2.0;
+
+        System.out.println(result);
+
+    }
+}
