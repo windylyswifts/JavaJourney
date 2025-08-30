e2e0c7b20faa49e948696f9bf1ad6d408d0b62e4
windylyswifts <isiahalinab@gmail.com>
2025-08-30 19:56:04 +0800

    forgot to pull this

---
+++ Lesson13_string_methods/L13.java
@@ -0,0 +1,12 @@
+package Lesson13_string_methods;
+
+public class L13 {
+    public static void main(String[] args) {
+        String name = "isiah alinab";
+
+        int length = name.length(); //to identify the length of the string
+        
+
+        System.out.println(length);
+    }
+}
