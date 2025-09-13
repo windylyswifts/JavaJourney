43f409c93ddcaed3a8f15c615e225fb3cc33d0bb
windylyswifts <isiahalinab@gmail.com>
2025-09-13 08:24:12 +0800

    finished lesson L14

--- Lesson14_substring/L14.java
+++ Lesson14_substring/L14.java
@@ -1,15 +1,29 @@
 package Lesson14_substring;
+import java.util.Scanner;
 
 public class L14 {
     public static void main(String[] args) {
         // .substring() =  A method used to extract a portion of a string
         //                 string.substring(start, end)
+        Scanner scanner = new Scanner(System.in);
 
-        String email = "windsdds@gmail.com";
-        String username = email.substring(0, 6);
-        String domain = email.substring(4, 6);
+        String email;
+        String username;
+        String domain;
 
-        System.out.println(username);
-        System.out.println(domain);
+        System.out.println("What is your email:");
+        email = scanner.nextLine();
+
+        if (email.contains("@")) {
+            username = email.substring(0, email.indexOf("@")); //this will end on @
+            domain = email.substring(email.indexOf("@") + 1); //what this basically do is once @ is found plus 1 so it ignores @
+    
+            System.out.println(username);
+            System.out.println(domain);            
+        }
+        else{
+            System.out.println("That ain't an email!");
+        }
+        scanner.close();
     }
 }
